1551623013d76416979adf67b34f47a301390536
bubliks7 <bubliksiur@gmail.com>
2026-03-16 17:59:12 +0100

    added models etc.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from projects.models import Project
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ("todo", "To Do"),
+        ("in_progress", "In Progress"),
+        ("done", "Done")
+    ]
+    
+    PRIORITY_CHOICES = [
+        ("low", "Low"),
+        ("medium", "Medium"),
+        ("high", "High")
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="todo")
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default="medium")
+    due_date = models.DateTimeField(null=True, blank=True)
+    created_at =  models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
