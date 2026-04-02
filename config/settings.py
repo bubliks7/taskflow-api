f38a4d8831dacfb4df145dfd0c9279b9e9076f24
bubliks7 <bubliksiur@gmail.com>
2026-04-03 00:36:26 +0200

    add JWT login with Auth

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dj_database_url
+from datetime import timedelta
 
 load_dotenv()
 
@@ -127,3 +128,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+}
--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Task
-from .serializers import TaskSerializers
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 class TaskListView(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializers
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(assigned_to=self.request.user)
+    
+    def get_queryset(self):
+        return Task.objects.filter(assigned_to=self.request.user)
 
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializers
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
